15d3dc4242985fd991ecfb17d3f7dcc3caa6cc62
Patrick Stephen Jr <p.stephen@fetchrewards.com>
2024-10-10 10:52:01 -0500

    Update SwiftSyntaxSugar (#1)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 import CompilerPluginSupport
@@ -26,11 +25,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-syntax.git",
-            from: "509.0.2"
+            from: "600.0.1"
         ),
         .package(
             url: "git@github.com:fetch-rewards/SwiftSyntaxSugar.git",
-            branch: "refactor/SyntaxProtocol-with"
+            revision: "abfb9241a6efb9b329c6b056e739422d2c98d8d7"
         ),
     ],
     targets: [
@@ -69,80 +68,21 @@ let package = Package(
 
 // MARK: - Swift Settings
 
-/// For a list of all upcoming features, visit the [Swift Evolution Dashboard]
-/// (https://www.swift.org/swift-evolution/#?upcoming=true)
 extension SwiftSetting {
+    static let existentialAny: SwiftSetting = .enableUpcomingFeature(
+        "ExistentialAny"
+    )
 
-    // MARK: Upcoming Features
-
-    /// A Swift feature that adds the option to create regular expressions from
-    /// regular expression literals.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0354-regex-literals.md)
-    static let bareSlashRegexLiterals: SwiftSetting = .enableUpcomingFeature("BareSlashRegexLiterals")
-
-    /// A Swift feature that changes the evaluation of `#file` from a string
-    /// literal containing the current source file's full path to a
-    /// human-readable string containing the filename and module name, while
-    /// preserving the former behavior in a new `#filePath` expression.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0274-magic-file.md)
-    static let conciseMagicFile: SwiftSetting = .enableUpcomingFeature("ConciseMagicFile")
-
-    /// A Swift feature that disables global actor isolation inference for types
-    /// that contain property wrappers that are isolated to a global actor.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0401-remove-property-wrapper-isolation.md)
-    static let disableOutwardActorInference: SwiftSetting = .enableUpcomingFeature("DisableOutwardActorInference")
-
-    /// A Swift feature that enforces the use of the `any` keyword for
-    /// existential types.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0335-existential-any.md)
-    static let existentialAny: SwiftSetting = .enableUpcomingFeature("ExistentialAny")
-
-    /// A Swift feature that replaces backward-scan matching for multiple
-    /// trailing closures with forward-scan matching wherever possible.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0286-forward-scan-trailing-closures.md)
-    static let forwardTrailingClosures: SwiftSetting = .enableUpcomingFeature("ForwardTrailingClosures")
-
-    /// A Swift feature that improves the default API interface of imported
-    /// Objective-C code that uses forward declarations.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0384-importing-forward-declared-objc-interfaces-and-protocols.md)
-    static let importObjcForwardDeclarations: SwiftSetting = .enableUpcomingFeature("ImportObjcForwardDeclarations")
-
-    // MARK: Experimental Features
-
-    /// A Swift feature that enables strict concurrency checking.
-    ///
-    /// Although this is marked as an "experimental" feature, it is still safe
-    /// to enable. Strict concurrency checking is still evolving and is [not yet
-    /// in its final Swift 6 form](https://github.com/apple/swift/pull/66991)
-    /// like "upcoming" features are.
-    ///
-    /// [Swift Evolution proposal]
-    /// (https://github.com/apple/swift-evolution/blob/main/proposals/0337-support-incremental-migration-to-concurrency-checking.md)
-    static let strictConcurrency: SwiftSetting = .enableExperimentalFeature("StrictConcurrency")
+    static let internalImportsByDefault: SwiftSetting = .enableUpcomingFeature(
+        "InternalImportsByDefault"
+    )
 }
 
 extension Array where Element == SwiftSetting {
 
     /// Default Swift settings to enable for targets.
     static let `default`: [SwiftSetting] = [
-        .bareSlashRegexLiterals,
-        .conciseMagicFile,
-        .disableOutwardActorInference,
         .existentialAny,
-        .forwardTrailingClosures,
-        .importObjcForwardDeclarations,
-        .strictConcurrency,
+        .internalImportsByDefault,
     ]
 }
